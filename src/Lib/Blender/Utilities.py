42a1d3f801c5f638f9a90f80d50bc5c078d380c7
Roman Parak <Roman.Parak@outlook.com>
2023-07-07 11:36:00 +0200

    improvements of the repo.

---
+++ src/Lib/Blender/Utilities.py
@@ -0,0 +1,178 @@
+# BPY (Blender as a python) [pip3 install bpy]
+import bpy
+# Typing (Support for type hints)
+import typing as tp
+
+def Deselect_All() -> None:
+    """
+    Description:
+        Deselect all objects in the current scene.
+    """
+    
+    for obj in bpy.context.selected_objects:
+        bpy.data.objects[obj.name].select_set(False)
+    bpy.context.view_layer.update()
+        
+def Object_Exist(name: str) -> bool:
+    """
+    Description:
+        Check if the object exists within the scene.
+        
+    Args:
+        (1) name [string]: Object name.
+        
+    Returns:
+        (1) parameter [bool]: 'True' if it exists, otherwise 'False'.
+    """
+    
+    return True if bpy.context.scene.objects.get(name) else False
+
+def Remove_Object(name: str) -> None:
+    """
+    Description:
+        Remove the object (hierarchy) from the scene, if it exists. 
+
+    Args:
+        (1) name [string]: The name of the object.
+    """
+
+    # Find the object with the desired name in the scene.
+    object_name = None
+    for obj in bpy.data.objects:
+        if name in obj.name and Object_Exist(obj.name) == True:
+            object_name = obj.name
+            break
+
+    # If the object exists, remove it, as well as the other objects in the hierarchy.
+    if object_name is not None:
+        bpy.data.objects[object_name].select_set(True)
+        for child in bpy.data.objects[object_name].children:
+            child.select_set(True)
+        bpy.ops.object.delete()
+        bpy.context.view_layer.update()
+
+def Set_Object_Material_Transparency(name: str, alpha: float) -> None:
+    """
+    Description:
+        Set the transparency of the object material and/or the object hierarchy (if exists).
+        
+        Note: 
+            alpha = 1.0: Render surface without transparency.
+            
+    Args:
+        (1) name [string]: The name of the object.
+        (2) alpha [float]: Transparency information.
+                           (total transparency is 0.0 and total opacity is 1.0)
+    """
+
+    for obj in bpy.data.objects:
+        if bpy.data.objects[name].parent == True:
+            if obj.parent == bpy.data.objects[name]:
+                for material in obj.material_slots:
+                    if alpha == 1.0:
+                        material.material.blend_method  = 'OPAQUE'
+                    else:
+                        material.material.blend_method  = 'BLEND'
+                    
+                    material.material.shadow_method = 'OPAQUE'
+                    material.material.node_tree.nodes['Principled BSDF'].inputs['Alpha'].default_value = alpha
+                
+                # Recursive call.
+                return Set_Object_Material_Transparency(obj.name, alpha)
+        else:
+            if obj == bpy.data.objects[name]:
+                for material in obj.material_slots:
+                    if alpha == 1.0:
+                        material.material.blend_method  = 'OPAQUE'
+                    else:
+                        material.material.blend_method  = 'BLEND'
+                    
+                    material.material.shadow_method = 'OPAQUE'
+                    material.material.node_tree.nodes['Principled BSDF'].inputs['Alpha'].default_value = alpha
+
+def __Add_Primitive(type: str, properties: tp.Tuple[float, tp.List[float], tp.List[float]]) -> bpy.ops.mesh:
+    """
+    Description:
+        Add a primitive three-dimensional object.
+        
+    Args:
+        (1) type [string]: Type of the object. 
+                            Primitives: ['Plane', 'Cube', 'Sphere', 'Capsule']
+        (2) properties [Dictionary {'Size/Radius': float, 'Scale/Size/None': Vector<float>, 
+                                    'Location': Vector<float>]: Transformation properties of the created object. The structure depends 
+                                                                on the specific object.
+    
+    Returns:
+        (1) parameter [bpy.ops.mesh]: Individual three-dimensional object (primitive).
+    """
+        
+    return {
+        'Plane': lambda x: bpy.ops.mesh.primitive_plane_add(size=x['Size'], scale=x['Scale'], location=x['Location']),
+        'Cube': lambda x: bpy.ops.mesh.primitive_cube_add(size=x['Size'], scale=x['Scale'], location=x['Location']),
+        'Sphere': lambda x: bpy.ops.mesh.primitive_uv_sphere_add(radius=x['Radius'], location=x['Location']),
+        'Capsule': lambda x: bpy.ops.mesh.primitive_round_cube_add(radius=x['Radius'], size=x['Size'], location=x['Location'], arc_div=10)
+    }[type](properties)
+
+def Create_Primitive(type: str, name: str, properties: tp.Tuple[tp.Tuple[float, tp.List[float]], tp.Tuple[float]]) -> None:
+    """
+    Description:
+        Create a primitive three-dimensional object with additional properties.
+
+    Args:
+        (1) type [string]: Type of the object. 
+                            Primitives: ['Plane', 'Cube', 'Sphere', 'Capsule']
+        (2) name [string]: The name of the created object.
+        (3) properties [{'transformation': {'Size/Radius': float, 'Scale/Size/None': Vector<float>, Location': Vector<float>}, 
+                         'material': {'RGBA': Vector<float>, 'alpha': float}}]: Properties of the created object. The structure depends on 
+                                                                                on the specific object.
+    """
+
+    # Create a new material and set the material color of the object.
+    material = bpy.data.materials.new(f'{name}_mat')
+    material.use_nodes = True
+    material.node_tree.nodes['Principled BSDF'].inputs['Base Color'].default_value = properties['material']['RGBA']
+
+    # Add a primitive three-dimensional object.
+    __Add_Primitive(type, properties['transformation'])
+
+    # Change the name and material of the object.
+    bpy.context.active_object.name = name
+    bpy.context.active_object.active_material = material
+
+    # Set the transparency of the object material.
+    if properties['material']['alpha'] < 1.0:
+        Set_Object_Material_Transparency(name, properties['material']['alpha'])
+
+    # Deselect all objects in the current scene.
+    Deselect_All()
+
+    # Update the scene.
+    bpy.context.view_layer.update()
+
+def Get_Object_Hierarchy(name: str) -> None:
+    """
+    Description: 
+        Get the hierarchy of objects from the object structure.
+        
+    Args:
+        (1) name [string]: Name of the main object.
+
+    Example:
+        Get_Object_Hierarchy(bpy.data.objects['ABB_IRB_120'])
+    """
+    
+    for obj in bpy.data.objects:
+        if obj.parent == bpy.data.objects[name]:
+            # Print name of the current object (index i).
+            print(obj.name)
+            # Recursive call.
+            return Get_Object_Hierarchy(obj.name)
+
+def Remove_Animation_Data() -> None:
+    """
+    Description: 
+        Remove animation data from objects (Clear keyframes).
+    """
+    
+    for obj in bpy.data.objects:
+        obj.animation_data_clear()
